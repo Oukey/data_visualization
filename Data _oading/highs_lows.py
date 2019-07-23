6efa3d9662bc4d0d6f8e831a3bd3367f252bf8bc
Oukey <oukey311@gmail.com>
2019-07-23 22:35:39 +0400

    Визуализация данных (даты, температура). Добавлен график температурных минимумов и реализовано цветовое выделение части диаграммы.

--- Data _oading/highs_lows.py
+++ Data _oading/highs_lows.py
@@ -4,17 +4,20 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-# чтение дат и максимальных температур из файла
+# чтение дат и температурных максимумов и минимумов из файла
 filename = 'sitka_weather.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         dates.append(current_date)
         high = int(row[5])
         highs.append(high)
+
+        low = int(row[6])
+        lows.append(low)
 
         # for index, column_header in enumerate(header_row):
         #     print(index, column_header)
@@ -30,10 +33,12 @@ with open(filename) as f:
 
 # Нанесение данных на диаграмму
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диграммы
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high and lows temperatures, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
