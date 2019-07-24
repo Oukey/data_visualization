e6a2cbc2864fc50d8ee2d0345f907f93b43a5266
Oukey <oukey311@gmail.com>
2019-07-24 21:35:29 +0400

    Проверка ошибок и обработка исключений

--- Data _oading/highs_lows_02.py
+++ Data _oading/highs_lows_02.py
@@ -1,35 +1,26 @@
-# highs_Lows.py
+# highs_lows_02.py
 
 import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
 # чтение дат и температурных максимумов и минимумов из файла
-filename = 'sitka_weather.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        dates.append(current_date)
-        high = int(row[5])
-        highs.append(high)
-
-        low = int(row[6])
-        lows.append(low)
-
-        # for index, column_header in enumerate(header_row):
-        #     print(index, column_header)
-        '''
-        0 STATION
-        1 NAME
-        2 DATE
-        3 PRCP
-        4 TAVG
-        5 TMAX
-        6 TMIN
-        '''
+        try:
+            current_date = datetime.strptime(row[0], '%Y-%m-%d')
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Нанесение данных на диаграмму
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -38,7 +29,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диграммы
-plt.title("Daily high and lows temperatures, July 2018", fontsize=24)
+title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
@@ -46,4 +38,4 @@ plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
 
-#354
+# 354
