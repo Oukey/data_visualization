d47872ca74c64e976863538e1a2c06a82e4ff4a4
Oukey <oukey311@gmail.com>
2019-08-02 22:38:59 +0400

    rework

--- API/python_repos.py
+++ API/python_repos.py
@@ -15,18 +15,33 @@ print('Total repositories:', response_dict['total_count'])  # вывод общ�
 
 # Анализ информации о репозиториях
 repo_dicts = response_dict['items']
+print('number of items:', len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],  # количество звезд
+        'label': repo_dict['description'],  # описание проекта
+    }
+    plot_dicts.append(plot_dict)
 
 # Построение визуализации
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_dont_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15  # сокращение длинных имен до 15 символов
+my_config.show_y_guides = False  # скрытие горизонтальных линий на графике
+my_config.width = 1000  # ширина диаграммы
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
 
 
@@ -50,3 +65,5 @@ chart.render_to_file('python_repos.svg')
 # print('\nKeys:', len(repo_dict))
 # for key in sorted(repo_dict.keys()):
 #     print(key)
+
+# 370
